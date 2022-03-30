4f079cd38d129c0855081be1cc707b4d6281e1e8
K-CjaeHyun <cjwogus1225@korea.ac.kr>
2022-03-31 02:50:24 +0900

    초기 커밋.

---
+++ input project/p1.c
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+#include <stdio.h>
+
+int main() {
+	char A;
+
+	printf("한 문자 입력: ");
+	scanf(" %c", &A);
+
+	printf("출력: %c \n", A);
+
+
+}
